79d0c0e1c77e501912cc671dfba94cfa1fe343e5
kenya12814 <85816730+KenyaMasuko@users.noreply.github.com>
2023-11-25 21:51:18 +0900

    init

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,7 @@
+// NOTE2: @の意味を説明する
+import '@/app/ui/global.css';
+import { inter, lusitana } from '@/app/ui/fonts';
+
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +9,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
